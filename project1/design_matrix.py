08f0802a8ab703be41e52d8e7ced6373c83a8a4e
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-09-06 12:46:04 +0200

    Minor changes

--- project1/design_matrix.py
+++ project1/design_matrix.py
@@ -1,24 +1,23 @@
 import numpy as np
 from scipy.special import factorial
 
-def construct_design_matrix(x, y, N):
+def design_matrix(x, y, N):
     """
     Returns the design matrix that models
     the output z = f(x, y) as a 2-variable polynomial of degree N.
 
     x, y: Arrays of input variables (must have same length!)
-    N: Degree of polynomial
+    N:    Degree of polynomial
 
-    To display this message in the terminal use help(construct_design_matrix)
     """
 
 
-    p = factorial(N+2)/(factorial(N)*factorial(2)) # Number of columns in the design matrix,
-                                                   # i.e the number of terms in the polynomial
-    n = len(x)                                     # Number of rows in the design matrix
+    cols = factorial(N+2)/(factorial(N)*factorial(2)) # Number of columns in the design matrix,
+                                                      # i.e the number of terms in the polynomial
+    rows = len(x)                                     # Number of rows in the design matrix
 
-    X = np.zeros((n, int(p)))
-    
+    X = np.zeros((rows, cols))
+
     col = 0
     for j in range(N+1):
         for i in range(N+1):
@@ -40,7 +39,7 @@ x = np.random.rand(3) # Only 3 data points is enough
 y = np.random.rand(3) # A first degree polynomial is also the equation for a plane
                       # A plane is uniquely defined by 3 points
 
-X = construct_design_matrix(x, y, 1)
+X = design_matrix(x, y, 1)
 
 z = np.zeros(len(x))  # Generate a "data set", output z from inputs x and y
 z = f(x, y)
