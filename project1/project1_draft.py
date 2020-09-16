0e330d5654bb288d37df3a0b0a04fac6ed070535
ilseku <54395297+ilseku@users.noreply.github.com>
2020-09-16 15:21:34 +0200

    Update project1_draft.py

--- project1/project1_draft.py
+++ project1/project1_draft.py
@@ -6,7 +6,7 @@ from matplotlib import cm
 from sklearn.utils import resample
 
 class project1:
-    def __init__(self, seed=4):
+    def __init__(self, seed=1):
         np.random.seed(seed)
         pass
 
@@ -38,16 +38,17 @@ class project1:
 
     def OLS(self, X, f):
         #beta = np.linalg.inv(X.T @ X) @ X.T @ f #this is much slower
-        beta =np.linalg.pinv(X) @ f
+        beta = np.linalg.pinv(X) @ f
         fpred = X @ beta
-        return beta, fpred
+        beta_var = np.linalg.inv(X.T.dot(X))
+        return beta, fpred, beta_var
 
-    def generate_data(self, n, m, noise=False):
+    def generate_data(self, n, m, noise):
         x = np.random.uniform(0, 1, n)
         y = np.random.uniform(0, 1, m)
         f_data = self.FrankeFunction(x, y)
         if noise == True:
-            f_data = f_data + np.random.normal(0, 1)
+            f_data_n = f_data + np.random.randn(len(f_data))
         return x, y, f_data
 
     def splitandscale(self, X, f_data):
@@ -58,81 +59,72 @@ class project1:
         X_test_scaled = scaler.transform(X_test)
         return X_train, X_test, f_train, f_test
 
-    def bootstrap(self, f_data, X, N_bootstrap, degree):
+    def bootstrap(self, datapoints, N_bootstrap, maxdegree):
+        bias = np.zeros(maxdegree)
+        variance = np.zeros(maxdegree)
+        MSE = np.zeros(maxdegree)
+        x, y, f_data = self.generate_data(datapoints, datapoints, False)
         for degree in range(maxdegree):
-            f_predict = np.empty((f_train.shape[0], N_bootstrap))
-            f_predict_test = np.empty((f_test.shape[0], N_bootstrap))
+            X = self.design_matrix(x, y, degree)
+            X_train, X_test, f_train, f_test = self.splitandscale(X, f_data)
+            f_predict = np.empty((f_test.shape[0], N_bootstrap))
             for i in range(N_bootstrap):
-                X_train, X_test, f_train, f_test = self.splitandscale(X, f_data)
                 X_, f_ = resample(X_train, f_train)
-
-                f_predict[:,i] = self.OLS(X_, f_)[1]
-                f_predict_test[:,i] = self.OLS(X_, f_)[1]
-        return y_predict
+                f_predict[:,i] = X_test @ self.OLS(X_, f_)[0]
+            bias[degree] = np.mean((f_test - np.mean(f_predict, axis=1))**2)
+            variance[degree] = np.mean(np.var(f_predict, axis=1))
+            #MSE[degree] = self.mean_squared_error(f_data, f_predict)
+            MSE[degree] = np.mean(np.mean((f_test - f_predict.T)**2 , axis=1))
+        return  bias, variance, MSE
 
 
 
 if __name__ == '__main__':
+    P1 = project1()
+    x, y, f_data = P1.generate_data(500, 500, False)
+    X = P1.design_matrix(x, y, 5)
+    beta, f_model, beta_var = P1.OLS(X, f_data)
+    print("Mean Squared Error: ")
+    print(P1.mean_squared_error(f_data, f_model))
+    print("R2 score: ")
+    print(P1.R_squared(f_data, f_model))
+    # print("Beta variance: ")
+    # print(beta_var)
+
     degrees = np.arange(0, 30, 1)
     MSEtest = np.zeros(len(degrees))
     MSEtrain = np.zeros(len(degrees))
-
     for i in degrees:
-        P1 = project1()
-        x, y, f_data = P1.generate_data(100, 100, False)
-        X = P1.design_matrix(x, y, degrees[i])
-
-        #not splitted into test and train
-        # beta, f_model = P1.OLS(X, f_data)
-        # print("Mean Squared Error: ")
-        # print(P1.mean_squared_error(f_data, f_model))
-        # print("R2 score: ")
-        # print(P1.R_squared(f_data, f_model))
-
-
+        X = P1.design_matrix(x, y, i)
         X_train_scaled, X_test_scaled, f_train, f_test = P1.splitandscale(X, f_data)
-        beta, f_train_model = P1.OLS(X_train_scaled, f_train)
+        beta, f_train_model, beta_var = P1.OLS(X_train_scaled, f_train)
         f_test_model = X_test_scaled @ beta
-        print("Training R2 for OLS")
-        print(P1.R_squared(f_train, f_train_model))
-        print("Training MSE for OLS")
-        print(P1.mean_squared_error(f_train, f_train_model))
         MSEtrain[i] = P1.mean_squared_error(f_train, f_train_model)
-        print("Test R2 for OLS")
-        print(P1.R_squared(f_test, f_test_model))
-        print("Test MSE for OLS")
-        print(P1.mean_squared_error(f_test, f_test_model))
         MSEtest[i] = P1.mean_squared_error(f_test, f_test_model)
 
-    print(MSEtest)
-    print(MSEtrain)
-    plt.plot(degrees, np.log(MSEtest), label='test')
-    plt.plot(degrees, np.log(MSEtrain), label='train')
+    plt.plot(degrees, MSEtest, label='test')
+    plt.plot(degrees, MSEtrain, label='train')
     plt.legend()
+    plt.yscale('log')
+    plt.show()
+
+
+    maxdegree = 15
+    n_boot = 10
+    datapoints = 500
+    bias, variance, MSE = P1.bootstrap(datapoints, n_boot, maxdegree)
+    polydegree = np.arange(maxdegree)
+    plt.plot(polydegree, bias, label='Bias')
+    plt.plot(polydegree, variance, label='var')
+    plt.plot(polydegree, MSE, label='MSE')
+    plt.legend()
+    # plt.yscale('log')
     plt.show()
 
 
 
 
-#   beginning of the bootstrap method, not ready yet
-#     N_bootstrap = 100
-#     y_pred = P1.bootstrap(f_data, X, N_bootstrap, degree)
-#
-#     # complexity = np.arange(0, 10, 1)
-#     # prediction_error = np.zeros(len(complexity))
-#     # for i in range(len(complexity)):
-#     #     X2 = P1.design_matrix(x, y, i)
-#     #     x_train, x_test, f_train2, f_test2 = P1.splitandscale(X2, f_data)
-#     #     beta2, f_train_model2 = P1.OLS(x_train, f_train2)
-#     #     f_test_model2 = x_test @ beta2
-#     #     prediction_error_train[i] = P1.mean_squared_error(f_train2, f_train_model2)
-#     #     prediction_error_test[i] = f_data -
-#     # print(prediction_error.shape)
-#     # print(complexity.shape)
-#     # plt.plot(complexity, prediction_error)
-#     # plt.show()
-#
-#
+
 # # plotting the data and the model
 #     x1, x2 = np.meshgrid(np.sort(x), np.sort(y))
 #     f = P1.FrankeFunction(x1, x2)
