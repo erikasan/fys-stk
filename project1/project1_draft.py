dfc160115edbecb87e89db036c2b5c9618b94e5f
ilseku <54395297+ilseku@users.noreply.github.com>
2020-09-22 19:24:55 +0200

    Update project1_draft.py

--- project1/project1_draft.py
+++ project1/project1_draft.py
@@ -21,7 +21,6 @@ class project1:
         n = np.size(y_model)
         return 1/n*np.sum((y_data - y_model)**2)
 
-
     def R_squared(self, y_data, y_model):
         return 1 - np.sum((y_data - y_model)**2)/np.sum((y_data - np.mean(y_data))**2)
 
@@ -39,9 +38,9 @@ class project1:
         beta = np.linalg.pinv(X) @ f
         return beta
 
-    def beta_var_OLS(self, X, f_data, f_model):
+    def beta_var_OLS(self, X, f_data, f_model, p):
         N = len(f_data)
-        sigma = 1/(N)*np.sum((f_data - f_model)**2)
+        sigma = 1/(N-p-1)*np.sum((f_data - f_model)**2)
         var_beta = np.linalg.inv(X.T @ X)*sigma**2
         var_beta = np.diag(var_beta)
         return var_beta
@@ -56,24 +55,26 @@ class project1:
 
     def scale(self, X_train, X_test):
         scaler = StandardScaler()
-        scaler.fit(X_train)
-        X_train_scaled = scaler.transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
-        return X_train, X_test
+        scaler.fit(X_train[:,1:])
+        X_train_scaled = np.ones(X_train.shape)
+        X_test_scaled = np.ones(X_test.shape)
+        X_train_scaled[:,1:] = scaler.transform(X_train[:, 1:])
+        X_test_scaled[:,1:] = scaler.transform(X_test[:,1:])
+        return X_train_scaled, X_test_scaled
 
     def bootstrap(self, datapoints, N_bootstrap, maxdegree, noise):
         bias = np.zeros(maxdegree)
         variance = np.zeros(maxdegree)
         MSE = np.zeros(maxdegree)
         x, y, f_data = self.generate_data(datapoints, datapoints, noise)
-        for degree in range(maxdegree):
+        for degree in range(1, maxdegree):
             X = self.design_matrix(x, y, degree)
             X_train, X_test, f_train, f_test = train_test_split(X, f_data, test_size=0.2)
-            #need to scale ?
+            X_train, X_test = self.scale(X_train, X_test)
             f_predict = np.empty((f_test.shape[0], N_bootstrap))
             for i in range(N_bootstrap):
                 X_, f_ = resample(X_train, f_train)
-                f_predict[:,i] = X_test @ self.OLS(X_, f_)[0]
+                f_predict[:,i] = X_test @ self.OLS(X_, f_)
             bias[degree] = np.mean((f_test - np.mean(f_predict, axis=1))**2)
             variance[degree] = np.mean(np.var(f_predict, axis=1))
             MSE[degree] = np.mean(np.mean((f_test - f_predict.T)**2 , axis=1))
@@ -104,81 +105,89 @@ class project1:
 
 if __name__ == '__main__':
     P1 = project1()
-    x, y, f_data = P1.generate_data(500, 500, False)
-    X = P1.design_matrix(x, y, 5)
-    beta = P1.OLS(X, f_data)
-    f_model = X @ beta
-    print("Mean Squared Error: ")
-    print(P1.mean_squared_error(f_data, f_model))
-    print("R2 score: ")
-    print(P1.R_squared(f_data, f_model))
-    beta_var = P1.beta_var_OLS(X, f_data, f_model)
-    print("Beta variance: ")
-    print(beta_var)
-
-def plotting_test_vs_train():
-    P1 = project1()
-    x, y, f_data = P1.generate_data(500, 500, False)
-    degrees = np.arange(1, 30, 1)
-    MSEtest = np.zeros(len(degrees))
-    MSEtrain = np.zeros(len(degrees))
-    for i in degrees-1:
-        X = P1.design_matrix(x, y, i)
+    def calcOLS():
+        degree = 5
+        x, y, f_data = P1.generate_data(500, 500, False)
+        X = P1.design_matrix(x, y, degree)
         X_train, X_test, f_train, f_test = train_test_split(X, f_data, test_size=0.2)
-        X_train_scaled, X_test_scaled = P1.scale(X_train, X_test)
-        beta, f_train_model, beta_var = P1.OLS(X_train_scaled, f_train)
-        f_test_model = X_test_scaled @ beta
-        MSEtrain[i] = P1.mean_squared_error(f_train, f_train_model)
-        MSEtest[i] = P1.mean_squared_error(f_test, f_test_model)
-    plt.plot(degrees, MSEtest, label='test')
-    plt.plot(degrees, MSEtrain, label='train')
-    plt.legend()
-    plt.yscale('log')
-    plt.show()
+        X_train, X_test = P1.scale(X_train, X_test)
+        beta = P1.OLS(X_train, f_train)
+        f_model = X_test @ beta
+        print("Mean Squared Error: ")
+        print(P1.mean_squared_error(f_test, f_model))
+        print("R2 score: ")
+        print(P1.R_squared(f_test, f_model))
+        beta_var = P1.beta_var_OLS(X_test, f_test, f_model, degree)
+        print("Beta variance: ")
+        print(beta_var)
+
+    calcOLS()
+
+    def plotting_test_vs_train():
+        P1 = project1()
+        x, y, f_data = P1.generate_data(500, 500, False)
+        degrees = np.arange(30)
+        MSEtest = np.zeros(len(degrees))
+        MSEtrain = np.zeros(len(degrees))
+        for i in range(1, len(degrees)):
+            X = P1.design_matrix(x, y, i)
+            X_train, X_test, f_train, f_test = train_test_split(X, f_data, test_size=0.2)
+            X_train_scaled, X_test_scaled = P1.scale(X_train, X_test)
+            beta = P1.OLS(X_train_scaled, f_train)
+            f_test_model = X_test_scaled @ beta
+            f_train_model = X_train_scaled @ beta
+            MSEtrain[i] = P1.mean_squared_error(f_train, f_train_model)
+            MSEtest[i] = P1.mean_squared_error(f_test, f_test_model)
+        plt.plot(degrees, MSEtest, label='test')
+        plt.plot(degrees, MSEtrain, label='train')
+        plt.legend()
+        plt.yscale('log')
+        plt.show()
+    plotting_test_vs_train()
+
+    def plotting_bootstrap():
+        P1 = project1()
+        maxdegree = 25
+        n_boot = 50
+        datapoints = 1000
+        bias, variance, MSE = P1.bootstrap(datapoints, n_boot, maxdegree, noise=False)
+        polydegree = np.arange(maxdegree)
+        plt.plot(polydegree, bias, label='Bias')
+        plt.plot(polydegree, variance, label='var')
+        plt.plot(polydegree, MSE, label='MSE')
+        plt.legend()
+        plt.yscale('log')
+        plt.show()
+    plotting_bootstrap()
+
+    def plotting_kfold():
+        degrees = np.arange(25)
+        R2 = np.zeros(len(degrees))
+        MSE = np.zeros(len(degrees))
+        k = 5
+        for degree in degrees:
+            MSE[degree], R2[degree] = P1.kfold(500, k, degree, False)
+        plt.plot(degrees, MSE)
+        plt.show()
+
+    plotting_kfold()
 
 
-def plotting_bootstrap():
-    P1 = project1()
-    maxdegree = 25
-    n_boot = 50
-    datapoints = 1000
-    bias, variance, MSE = P1.bootstrap(datapoints, n_boot, maxdegree, noise=False)
-    polydegree = np.arange(maxdegree)
-    plt.plot(polydegree, bias, label='Bias')
-    plt.plot(polydegree, variance, label='var')
-    plt.plot(polydegree, MSE, label='MSE')
-    plt.legend()
-    plt.yscale('log')
-    plt.show()
+    def plotting_function():
+        x1, x2 = np.meshgrid(np.sort(x), np.sort(y))
+        f = P1.FrankeFunction(x1, x2)
+        fpred = P1.OLS(X, f)[1]
+        fplot = fpred.reshape(100, 100)
 
-def plotting_kfold():
-    degrees = np.arange(25)
-    R2 = np.zeros(len(degrees))
-    MSE = np.zeros(len(degrees))
-    k = 5
-    for degree in degrees:
-        MSE[degree], R2[degree] = P1.kfold(500, k, degree, False)
-    plt.plot(degrees, MSE)
-    plt.show()
+        fig = plt.figure()
 
-plotting_kfold()
+        ax = fig.add_subplot(1, 2, 1, projection ='3d')
+        surf = ax.plot_surface(x1, x2, f, cmap=cm.viridis, linewidth=0, antialiased=False)
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.title('Franke')
 
-
-def plotting_function():
-    x1, x2 = np.meshgrid(np.sort(x), np.sort(y))
-    f = P1.FrankeFunction(x1, x2)
-    fpred = P1.OLS(X, f)[1]
-    fplot = fpred.reshape(100, 100)
-
-    fig = plt.figure()
-
-    ax = fig.add_subplot(1, 2, 1, projection ='3d')
-    surf = ax.plot_surface(x1, x2, f, cmap=cm.viridis, linewidth=0, antialiased=False)
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.title('Franke')
-
-    ax = fig.add_subplot(1, 2, 2, projection='3d')
-    surf = ax.plot_surface(x1, x2, fplot, cmap=cm.viridis, linewidth=0, antialiased=False)
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    plt.title('Franke fitted')
-    plt.show()
+        ax = fig.add_subplot(1, 2, 2, projection='3d')
+        surf = ax.plot_surface(x1, x2, fplot, cmap=cm.viridis, linewidth=0, antialiased=False)
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.title('Franke fitted')
+        plt.show()
