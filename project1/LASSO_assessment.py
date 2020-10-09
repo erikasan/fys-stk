a20a6a5f23174f6aa030df8188178c5250195094
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-10-09 18:26:11 +0200

    Minor changes

--- project1/LASSO_assessment.py
+++ project1/LASSO_assessment.py
@@ -11,87 +11,76 @@ from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 
 np.random.seed(16091995)
 
-n_datapoints = 10
-bootstraps = 3
+n_datapoints = 1000
+bootstraps = 100
 
 x = np.random.rand(n_datapoints)
 y = np.random.rand(n_datapoints)
 z = FrankeFunction(x, y) + 0.05*np.random.normal(0, 1, n_datapoints)
 
 
-p_min = 5
-p_max = 10
+p_min = 18
+p_max = 32
 polynomial_degrees = np.arange(p_min, p_max + 1, 1)
 
-lambdas = np.logspace(-6, -8, 10)
+lambdas = np.logspace(-16, -5, 15)
 
 x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x, y, z, test_size = 0.2)
 
-bias     = np.zeros((lambdas.size, polynomial_degrees.size))
-variance = np.zeros((lambdas.size, polynomial_degrees.size))
-MSE      = np.zeros((lambdas.size, polynomial_degrees.size))
 
-# for j, p in enumerate(polynomial_degrees):
-#     X = design_matrix(x, y, p, pandas = False)
-#     for i, lam in enumerate(lambdas):
-#         lasso_reg = Lasso(alpha = lam)
-#         MSE[i, j] = np.mean(-cross_val_score(lasso_reg, X, z, scoring = 'neg_mean_squared_error'))
+
+scaler = StandardScaler()
+
+
+bias      = np.zeros((lambdas.size, polynomial_degrees.size))
+variance  = np.zeros((lambdas.size, polynomial_degrees.size))
+MSE_cross = np.zeros((lambdas.size, polynomial_degrees.size))
+MSE_boot  = np.zeros((lambdas.size, polynomial_degrees.size))
 
 for j, p in enumerate(polynomial_degrees):
-    z_predict = np.zeros((z_test.shape[0], bootstraps))
-    X_train   = design_matrix(x_train, y_train, p, pandas = False)
-    X_test    = design_matrix(x_test, y_test, p, pandas = False)
-
+    X_scaled        = design_matrix(x, y, p, pandas = False)
+    #X_scaled = scaler.fit_transform(X)
     for i, lam in enumerate(lambdas):
-        lasso_reg = Lasso(alpha = lam)
+        lasso_reg       = Lasso(alpha = lam)#, warm_start = True, precompute = True)
+        MSE_cross[i, j] = np.mean(-cross_val_score(lasso_reg, X_scaled, z, scoring = 'neg_mean_squared_error'))
 
-        for B in range(bootstraps):
-            X_, z_                = resample(X_train, z_train)
-            lasso_reg.fit(X_, z_)
-            z_predict[:, B]       = lasso_reg.predict(X_test)
-
-        bias[i, j]     = np.mean((z_test - np.mean(z_predict, axis=1, keepdims=True))**2)
-        variance[i, j] = np.mean(np.var(z_predict, axis=1, keepdims=True))
-
-# Plot MSE
-
-# vmin = MSE.min()
-# vmax = MSE.max()
-# #vmax = 0.006
+# for j, p in enumerate(polynomial_degrees):
+#     z_predict      = np.zeros((z_test.shape[0], bootstraps))
+#     X_train        = design_matrix(x_train, y_train, p, pandas = False)
+#     X_test         = design_matrix(x_test, y_test, p, pandas = False)
+#     X_train_scaled = scaler.fit_transform(X_train)
+#     X_test_scaled  = scaler.fit_transform(X_test)
 #
-# sns.set()
+#     for i, lam in enumerate(lambdas):
+#         lasso_reg = Lasso(alpha = lam)#, warm_start = True, precompute = True)
 #
-# MSE = pd.DataFrame(MSE)
+#         for B in range(bootstraps):
+#             X_, z_                = resample(X_train_scaled, z_train)
+#             lasso_reg.fit(X_, z_)
+#             z_predict[:, B]       = lasso_reg.predict(X_test_scaled)
 #
-#
-# sns.heatmap(MSE,
-#             square      = True,
-#             xticklabels = polynomial_degrees,
-#             yticklabels = np.round(np.log10(lambdas), 2),
-#             cmap        = 'rainbow',
-#             vmin        = vmin,
-#             vmax        = vmax)
-#
-# plt.xlabel(r'Polynomial degree')
-# plt.ylabel(r'$\log( \lambda )$')
-#
-# plt.show()
+#         bias[i, j]     = np.mean((z_test - np.mean(z_predict, axis=1, keepdims=True))**2)
+#         variance[i, j] = np.mean(np.var(z_predict, axis=1, keepdims=True))
+#         MSE_boot[i, j] = mean_squared_error(z_test, np.mean(z_predict, axis=1, keepdims=True))
 
 
-# Plot bias
+#Plot MSE
 
-vmin = bias.min()
-vmax = bias.max()
-
-bias = pd.DataFrame(bias)
+vmin = MSE_cross.min()
+vmax = MSE_cross.max()
+#vmax = 0.006
 
 sns.set()
 
-sns.heatmap(bias,
+MSE = pd.DataFrame(MSE_cross)
+
+
+sns.heatmap(MSE_cross,
             square      = True,
             xticklabels = polynomial_degrees,
             yticklabels = np.round(np.log10(lambdas), 2),
@@ -103,6 +92,29 @@ plt.xlabel(r'Polynomial degree')
 plt.ylabel(r'$\log( \lambda )$')
 
 plt.show()
+
+
+# Plot bias
+
+# vmin = bias.min()
+# vmax = bias.max()
+#
+# bias = pd.DataFrame(bias)
+#
+# sns.set()
+#
+# sns.heatmap(bias,
+#             square      = True,
+#             xticklabels = polynomial_degrees,
+#             yticklabels = np.round(np.log10(lambdas), 2),
+#             cmap        = 'rainbow',
+#             vmin        = vmin,
+#             vmax        = vmax)
+#
+# plt.xlabel(r'Polynomial degree')
+# plt.ylabel(r'$\log( \lambda )$')
+#
+# plt.show()
 
 # Plot variance
 
