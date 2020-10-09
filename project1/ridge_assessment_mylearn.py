c13333843f4ff0d3f6d8cd00dc00a5b01b8aa1fd
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-10-09 18:25:44 +0200

    Minor changes

--- project1/ridge_assessment_mylearn.py
+++ project1/ridge_assessment_mylearn.py
@@ -11,6 +11,8 @@ from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+
 
 np.random.seed(16091995)
 
@@ -28,7 +30,7 @@ p_max = 20
 polynomial_degrees = np.arange(p_min, p_max + 1, 1)
 
 
-lambdas = np.logspace(-20, -1, 100)
+lambdas = np.logspace(-20, -1, 15)
 
 x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x, y, z, test_size = 0.2)
 
@@ -41,49 +43,70 @@ MSE_boot  = np.zeros((lambdas.size, polynomial_degrees.size))
 MSE_cross = np.zeros((lambdas.size, polynomial_degrees.size))
 
 
+
+
+
+# Bootstrap
+
+# for j, p in enumerate(polynomial_degrees):
+#     z_predict      = np.zeros((z_test.shape[0], bootstraps))
+#     X_train        = designMatrix(x_train, y_train, p, with_intercept = False)
+#     X_test         = designMatrix(x_test, y_test, p, with_intercept = False)
+#     X_train_scaled, X_test_scaled = normalize_data(X_train, X_test, with_intercept = False)
+#
+#     for i, lam in enumerate(lambdas):
+#         ridge_reg = RidgeRegression(lmbda=lam, fit_intercept=True)
+#         for B in range(bootstraps):
+#             X_, z_                = resample(X_train_scaled, z_train)
+#             ridge_reg.fit(X_, z_)
+#             z_predict[:, B]          = ridge_reg.predict(X_test_scaled)
+#
+#         bias[i, j]     = np.mean((z_test - np.mean(z_predict, axis=1, keepdims=True))**2)
+#         variance[i, j] = np.mean(np.var(z_predict, axis=1, keepdims=True))
+#         MSE_boot[i, j] = mean_squared_error(z_test, np.mean(z_predict, axis=1, keepdims=True))
+
 # Cross validation
 
 # for i, lam in enumerate(lambdas):
 #     for j, p in enumerate(polynomial_degrees):
 #         MSE_cross[i, j] = cross_validation(x, y, z, ridge, p, K = 5, lam = lam)
 
-
-# Bootstrap
-
+kfold = KFold(n_splits = 5)
 for j, p in enumerate(polynomial_degrees):
-    z_predict      = np.zeros((z_test.shape[0], bootstraps))
-    X_train        = designMatrix(x_train, y_train, p, with_intercept=False)
-    X_test         = designMatrix(x_test, y_test, p, with_intercept=False)
-    X_train_scaled, X_test_scaled = normalize_data(X_train, X_test, with_intercept=False)
-
+    X = designMatrix(x, y, p, with_intercept = False)
     for i, lam in enumerate(lambdas):
         ridge_reg = RidgeRegression(lmbda=lam, fit_intercept=True)
-        for B in range(bootstraps):
-            X_, z_                = resample(X_train_scaled, z_train)
-            ridge_reg.fit(X_, z_)
-            z_predict[:, B]          = ridge_reg.predict(X_test_scaled)
-            # beta            = ridge(X_, z_, lam
+        counter = 0
+        for train_inds, test_inds in kfold.split(x):
+            X_train = X[train_inds]
+            z_train = z[train_inds]
 
+            X_test  = X[test_inds]
+            z_test  = z[test_inds]
 
+            X_train_scaled, X_test_scaled = normalize_data(X_train, X_test, with_intercept = False)
+            z_train_scaled, z_test_scaled = normalize_target(z_train, z_test)
 
-            # model           = get_prediction(beta)
-            #z_predict[:, B] = model(X_test_scaled[:, 1], X_test_scaled[:, p+1])
+            ridge_reg.fit(X_train_scaled, z_train_scaled)
+            z_predict        = ridge_reg.predict(X_test_scaled)
+            MSE_cross[i, j] += mean_squared_error(z_predict, z_test_scaled)
 
-        bias[i, j]     = np.mean((z_test - np.mean(z_predict, axis=1, keepdims=True))**2)
-        variance[i, j] = np.mean(np.var(z_predict, axis=1, keepdims=True))
-        MSE_boot[i, j] = mean_squared_error(z_test, np.mean(z_predict, axis=1, keepdims=True))
+            counter += 1
+
+        MSE_cross[i, j] /= counter
+
 
 # Plot MSE
 
-vmin = MSE_boot.min()
-vmax = MSE_boot.max()
+vmin = MSE_cross.min()
+vmax = MSE_cross.max()
 #vmax = 0.006
 
-MSE_boot = pd.DataFrame(MSE_boot)
+MSE_cross = pd.DataFrame(MSE_cross)
 
 sns.set()
 
-sns.heatmap(MSE_boot,
+sns.heatmap(MSE_cross,
             square      = True,
             xticklabels = polynomial_degrees,
             yticklabels = np.round(np.log10(lambdas), 2),
