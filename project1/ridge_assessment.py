2437e96ce29a163b1e10553e5d589012e0a23910
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2020-10-10 13:31:11 +0200

    rip

--- project1/ridge_assessment.py
+++ project1/ridge_assessment.py
@@ -12,7 +12,7 @@ from FrankeFunction import FrankeFunction
 from sklearn.utils import resample
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-
+from sklearn.preprocessing import StandardScaler
 
 np.random.seed(16091995)
 
@@ -26,36 +26,49 @@ z = FrankeFunction(x, y) + 0.05*np.random.normal(0, 1, n_datapoints)
 
 
 p_min = 6
-p_max = 20
+p_max = 7
 polynomial_degrees = np.arange(p_min, p_max + 1, 1)
 
 
-lambdas = np.logspace(-16, -5, 20)
+lambdas = np.logspace(-16, -5, 3)
 
 x_train, x_test, y_train, y_test, z_train, z_test = train_test_split(x, y, z, test_size = 0.2)
 
-bias     = np.zeros((lambdas.size, polynomial_degrees.size))
-variance = np.zeros((lambdas.size, polynomial_degrees.size))
-MSE      = np.zeros((lambdas.size, polynomial_degrees.size))
+scaler = StandardScaler()
 
 
-# for i, lam in enumerate(lambdas):
-#     for j, p in enumerate(polynomial_degrees):
-#         MSE[i, j] = cross_validation(x, y, z, ridge, p, K = 5, lam = lam)
+bias      = np.zeros((lambdas.size, polynomial_degrees.size))
+variance  = np.zeros((lambdas.size, polynomial_degrees.size))
+MSE_boot  = np.zeros((lambdas.size, polynomial_degrees.size))
+MSE_cross = np.zeros((lambdas.size, polynomial_degrees.size))
 
+
+# Cross validation
+
+for i, lam in enumerate(lambdas):
+    for j, p in enumerate(polynomial_degrees):
+        MSE_cross[i, j] = cross_validation(x, y, z, ridge, p, K = 5, lam = lam)
+
+
+# Bootstrap
 
 for j, p in enumerate(polynomial_degrees):
-    z_predict = np.zeros((z_test.shape[0], bootstraps))
-    X = design_matrix(x_train, y_train, p, pandas = False)
+    z_predict      = np.zeros((z_test.shape[0], bootstraps))
+    X_train        = design_matrix(x_train, y_train, p, pandas = False)
+    X_test         = design_matrix(x_test, y_test, p, pandas = False)
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled  = scaler.fit_transform(X_test)
+
     for i, lam in enumerate(lambdas):
         for B in range(bootstraps):
-            X_, z_          = resample(X, z_train)
+            X_, z_          = resample(X_train_scaled, z_train)
             beta            = ridge(X_, z_, lam)
             model           = get_prediction(beta)
-            z_predict[:, B] = model(x_test, y_test)
+            z_predict[:, B] = model(X_test_scaled[:, 1], X_test_scaled[:, p+1])
 
         bias[i, j]     = np.mean((z_test - np.mean(z_predict, axis=1, keepdims=True))**2)
         variance[i, j] = np.mean(np.var(z_predict, axis=1, keepdims=True))
+        MSE_boot[i, j] = mean_squared_error(z_test, np.mean(z_predict, axis=1, keepdims=True))
 
 # Plot MSE
 
@@ -82,25 +95,25 @@ for j, p in enumerate(polynomial_degrees):
 
 # Plot bias
 
-vmin = bias.min()
-vmax = bias.max()
-
-bias = pd.DataFrame(bias)
-
-sns.set()
-
-sns.heatmap(bias,
-            square      = True,
-            xticklabels = polynomial_degrees,
-            yticklabels = np.round(np.log10(lambdas), 2),
-            cmap        = 'rainbow',
-            vmin        = vmin,
-            vmax        = vmax)
-
-plt.xlabel(r'Polynomial degree')
-plt.ylabel(r'$\log( \lambda )$')
-
-plt.show()
+# vmin = bias.min()
+# vmax = bias.max()
+#
+# bias = pd.DataFrame(bias)
+#
+# sns.set()
+#
+# sns.heatmap(bias,
+#             square      = True,
+#             xticklabels = polynomial_degrees,
+#             yticklabels = np.round(np.log10(lambdas), 2),
+#             cmap        = 'rainbow',
+#             vmin        = vmin,
+#             vmax        = vmax)
+#
+# plt.xlabel(r'Polynomial degree')
+# plt.ylabel(r'$\log( \lambda )$')
+#
+# plt.show()
 
 
 # Plot variance
